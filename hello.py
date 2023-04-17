4adf2800913ba6ef59ee1f59efdad4bdc4d57b87
kofi12 <kofi12@my.yorku.ca>
2023-04-17 15:12:45 -0400

    minor changes

---
+++ hello.py
@@ -0,0 +1,18 @@
+"""This assignment is to serve as an opportunity to learn how to write basic Python code
+, as well as laern how to commit/save your changes to Github. All assignments.
+
+Please fill in the following information:
+
+Name: <your name>
+Date Completed: <date>
+"""
+
+
+
+# write code below to print "Hello World" to the console
+def hello():
+    # your code goes here, remove the pass statement and replace it with your code
+    # hint - you can use the print() function to print to the console, all you need is one line!
+    pass
+
+hello()
